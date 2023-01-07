365ea1fed610c0d91e3d2e3bf977be8af010b049
AndrewLamWARC <91471887+AndrewLamWARC@users.noreply.github.com>
2023-01-07 23:15:04 +0000

    Add Easy type

---
+++ solutions/_00268-easy-if.ts
@@ -0,0 +1,19 @@
+type If<C extends boolean, T, F> = C extends true ? T : F
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
+  Expect<Equal<If<false, 'a', 2>, 2>>,
+]
+
+// @ts-expect-error
+type error = If<null, 'a', 'b'>
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/268/answer
+  > View solutions: https://tsch.js.org/268/solutions
+  > More Challenges: https://tsch.js.org
+*/
