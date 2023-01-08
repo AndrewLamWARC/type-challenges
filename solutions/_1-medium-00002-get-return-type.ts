f86f0843e385b946e265b96d8367435c44d4d955
AndrewLamWARC <91471887+AndrewLamWARC@users.noreply.github.com>
2023-01-08 17:27:14 +0000

    Add more alternative solution to ReturnType

--- solutions/_1-medium-00002-get-return-type.ts
+++ solutions/_1-medium-00002-get-return-type.ts
@@ -1,36 +1,48 @@
 // extends Function constrains T to a function type
 // (...args: any) => infer R : use expanded form of function type with infer to infer type of function return type
 type MyReturnType<T extends Function> = 
-  T extends (...args: any) => infer R
-    ? R
+  T extends (...args: any) => infer R 
+    ? R 
     : never
 
+// This does not work as you can pass non-function type as T
+type MyReturnType1<T> = 
+  T extends (...args: any) => infer R 
+    ? R 
+    : never
+
+// This may be arguably more correct since the args is an array of any
+type MyReturnType2<T extends Function> = 
+  T extends (...args: any[]) => infer R 
+    ? R 
+    : never
+  
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils"
 
 type cases = [
   Expect<Equal<string, MyReturnType<() => string>>>,
   Expect<Equal<123, MyReturnType<() => 123>>>,
   Expect<Equal<ComplexObject, MyReturnType<() => ComplexObject>>>,
   Expect<Equal<Promise<boolean>, MyReturnType<() => Promise<boolean>>>>,
-  Expect<Equal<() => 'foo', MyReturnType<() => () => 'foo'>>>,
+  Expect<Equal<() => "foo", MyReturnType<() => () => "foo">>>,
   Expect<Equal<1 | 2, MyReturnType<typeof fn>>>,
   Expect<Equal<1 | 2, MyReturnType<typeof fn1>>>,
-  
+
   // @ts-expect-error
   MyReturnType<{}>,
   // @ts-expect-error
-  MyReturnType<[]>,
+  MyReturnType<[]>
 ]
 
 type ComplexObject = {
-  a: [12, 'foo']
-  bar: 'hello'
+  a: [12, "foo"]
+  bar: "hello"
   prev(): number
 }
 
-const fn = (v: boolean) => v ? 1 : 2
-const fn1 = (v: boolean, w: any) => v ? 1 : 2
+const fn = (v: boolean) => (v ? 1 : 2)
+const fn1 = (v: boolean, w: any) => (v ? 1 : 2)
 
 /* _____________ Further Steps _____________ */
 /*
