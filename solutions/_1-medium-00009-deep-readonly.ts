ef80a6a2fcf384645e0b85af0e98d46478dddffc
AndrewLamWARC <91471887+AndrewLamWARC@users.noreply.github.com>
2023-01-09 11:08:55 +0000

    Add explanation to DeepReadonly why primitives are handled by mapped types

--- solutions/_1-medium-00009-deep-readonly.ts
+++ solutions/_1-medium-00009-deep-readonly.ts
@@ -7,6 +7,8 @@ type DeepReadonly<T> = {
 }
 
 // This works too. Although I don't fully understnd why primitive types are handled breaking recursion
+// Reason primitives are handled:
+// Mapped types already "skip" primitives by returning the input, and they automatically distribute over union, so you don't need to check for these yourself:
 type DeepReadonly01<T> = {
   readonly [U in keyof T]: T[U] extends Function
     ? T[U] // Special treatment for Function 
