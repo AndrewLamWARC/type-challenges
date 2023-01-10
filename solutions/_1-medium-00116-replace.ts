472f9808b8c8408c9489a7bf8bcee0b78a44590c
AndrewLamWARC <91471887+AndrewLamWARC@users.noreply.github.com>
2023-01-10 16:05:35 +0000

    Add Replace type

---
+++ solutions/_1-medium-00116-replace.ts
@@ -0,0 +1,28 @@
+// Edge case From = empty string
+type Replace<S extends string, From extends string, To extends string> = 
+  From extends ''
+    ? S
+    : S extends `${infer First}${From}${infer Rest}` 
+      ? `${First}${To}${Rest}` 
+      : S
+    
+type A1 = Replace<'foobarbar', '', 'foo'>
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', 'foo'>, 'foofoobar'>>,
+  Expect<Equal<Replace<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', ''>, 'foobar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'', '', ''>, ''>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/116/answer
+  > View solutions: https://tsch.js.org/116/solutions
+  > More Challenges: https://tsch.js.org
+*/
