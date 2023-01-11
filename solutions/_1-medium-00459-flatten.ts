da0f5da4529f50f058f210e72bcee848a565de1c
AndrewLamWARC <91471887+AndrewLamWARC@users.noreply.github.com>
2023-01-11 09:50:36 +0000

    Cleanup Flatten array

--- solutions/_1-medium-00459-flatten.ts
+++ solutions/_1-medium-00459-flatten.ts
@@ -1,22 +1,3 @@
-/*
-  459 - Flatten
-  -------
-  by zhouyiming (@chbro) #medium #array
-
-  ### Question
-
-  In this challenge, you would need to write a type that takes an array and emitted the flatten array type.
-
-  For example:
-
-  ```ts
-  type flatten = Flatten<[1, 2, [3, 4], [[[5]]]]> // [1, 2, 3, 4, 5]
-  ```
-
-  > View on GitHub: https://tsch.js.org/459
-*/
-
-/* _____________ Your Code Here _____________ */
 // Add type param T
 // Constrain T to array of unknowns
 // Add test for type error
