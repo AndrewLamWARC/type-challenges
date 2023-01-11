f18b43ae32cbf8c7be469ef0676f2d8d2f741fbf
AndrewLamWARC <91471887+AndrewLamWARC@users.noreply.github.com>
2023-01-11 09:24:11 +0000

    Add LengthOfString

---
+++ solutions/_1-medium-00298-length-of-string.ts
@@ -0,0 +1,26 @@
+// Convert string into tuple of characters. Store accumulated chars in type.
+// Then take length of tuple
+type LengthOfString<S extends string, Accumulate extends string[] = []> =
+  // Recursion stops when enire string consumed
+  S extends `${infer Car}${infer Cdr}`
+    ? LengthOfString<Cdr, [Car, ...Accumulate]>   
+    : Accumulate['length']
+
+type A1 = LengthOfString<''>
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<LengthOfString<''>, 0>>,
+  Expect<Equal<LengthOfString<'kumiko'>, 6>>,
+  Expect<Equal<LengthOfString<'reina'>, 5>>,
+  Expect<Equal<LengthOfString<'Sound! Euphonium'>, 16>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/298/answer
+  > View solutions: https://tsch.js.org/298/solutions
+  > More Challenges: https://tsch.js.org
+*/
