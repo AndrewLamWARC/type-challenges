69046eda8fec41b3336cb9c1b0f2d69e847ace66
andrewlam <andrew.lam@ascential.com>
2023-01-12 11:48:20 +0000

    Add IsNever type

---
+++ solutions/_1-medium-01042-isnever.ts
@@ -0,0 +1,26 @@
+type IsNever<T> = 
+  // Disable distributivity of conditional type otherwise test against never itself fails
+  // https://github.com/microsoft/TypeScript/issues/31751#issuecomment-498526919
+  [T] extends [never]
+    ? true
+    : false
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<IsNever<never>, true>>,
+  Expect<Equal<IsNever<never | string>, false>>,
+  Expect<Equal<IsNever<''>, false>>,
+  Expect<Equal<IsNever<undefined>, false>>,
+  Expect<Equal<IsNever<null>, false>>,
+  Expect<Equal<IsNever<[]>, false>>,
+  Expect<Equal<IsNever<{}>, false>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/1042/answer
+  > View solutions: https://tsch.js.org/1042/solutions
+  > More Challenges: https://tsch.js.org
+*/
